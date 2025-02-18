2064e8101e0e67b0c67b3e9520ccf9acecf1a831
tbeaumont79 <tbeaumont79@icloud.com>
2025-02-18 13:44:39 +0100

    feat: adding security package and the MessageService

---
+++ migrations/Version20250218123101.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250218123101 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE message (id SERIAL NOT NULL, author_id INT DEFAULT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6BD307FF675F31B ON message (author_id)');
+        $this->addSql('COMMENT ON COLUMN message.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON "user" (username)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FF675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FF675F31B');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
---
+++ src/Service/MessageService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Entity\Message;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+class MessageService { 
+    private $em;
+    private $hub;
+
+    public function __construct(EntityManagerInterface $em, HubInterface $hub) {
+        $this->em = $em;
+        $this->hub = $hub;
+    }
+
+    public function sendMessage(string $content, string $author) : void {
+        $message = new Message();
+        $message->setContent($content);
+        $message->setCreatedAt(new \DateTimeImmutable());
+        $foundedAuthor = $this->em->getRepository(User::class)->findOneBy(['username' => $author]);
+        $message->setAuthor($foundedAuthor);
+
+        $this->em->persist($message);
+        $this->em->flush();
+
+        $update = new Update(
+            'https://example.com/messages',
+            json_encode([
+                'status' => 'message successfuly sent',
+            ])
+            );
+            $this->hub->publish($update);
+    }
+}
