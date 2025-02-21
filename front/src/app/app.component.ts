8577f2c6df175f9bb72c4b50071a3e88d083608c
tbeaumont79 <tbeaumont79@icloud.com>
2025-02-21 01:44:27 +0100

    feat: adding the register component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-}
+export class AppComponent {}
