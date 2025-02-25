8a44e8bdaf833a9bfcbd8d77b2cf65f455b9b731
tbeaumont79 <tbeaumont79@icloud.com>
2025-02-25 16:42:52 +0100

    feat: start the http post in the front

--- back/src/Controller/RegistrationController.php
+++ back/src/Controller/RegistrationController.php
@@ -21,13 +21,15 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $em, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
     {
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        dd($user);
         try {
             $validator->validate($user);
         } catch (\Exception $e) {
             return $this->json(['message' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        //$form = $this->createForm(RegistrationFormType::class, $user);
         $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPlainPassword()));
+        dd($user);
         $em->persist($user);
         $em->flush();
         return $this->json(['message' => 'User successfully registered'], Response::HTTP_CREATED);
--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient) {}
+  register() {
+    this.http.post('http://localhost:8000/register', {
+      email: this.email,
+      password: this.password,
+    });
+  }
+}
