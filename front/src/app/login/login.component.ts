4db914aaca9e0776b1def443ff711e568b5742f8
tbeaumont79 <tbeaumont79@icloud.com>
2025-03-02 18:51:28 +0100

    feat: AuthService Work fine + adding a storage service for typescript and SSR

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { throwError, catchError } from 'rxjs';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -11,9 +13,21 @@ import { RouterLink } from '@angular/router';
 export class LoginComponent {
   email = '';
   password = '';
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   login() {
+    this.authService
+      .login(this.email, this.password)
+      .pipe(
+        catchError((error) => {
+          console.error('Login failed', error);
+          return throwError(() => error);
+        })
+      )
+      .subscribe({
+        next: (response) => console.log('Login successful', response),
+        error: (err) => console.error('Login failed', err),
+      });
     console.log(this.email, this.password);
   }
 }
--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap, BehaviorSubject, catchError, throwError } from 'rxjs';
+import { StorageService } from './storage.service';
 @Injectable({
   providedIn: 'root',
 })
@@ -8,42 +9,46 @@ export class AuthService {
   private API_URL = 'http://localhost:8000/api';
   private currentUserSubject = new BehaviorSubject<any>(null);
   public currentUser = this.currentUserSubject.asObservable();
-  constructor(private http: HttpClient) {
-    const token = localStorage.getItem('token');
-    const user = localStorage.getItem('user');
+  constructor(
+    private http: HttpClient,
+    private storageService: StorageService
+  ) {
+    const token = this.storageService.getItem('token');
+    const user = this.storageService.getItem('user');
     if (token && user) {
       this.currentUserSubject.asObservable();
     }
   }
-
   register(email: string, password: string): Observable<any> {
     const username = email.split('@')[0];
-    return this.http.post('{API_URL}/register', { username, password }).pipe(
-      tap((response) => {
-        console.log('Registration successful', response);
-      }),
-      tap((error) => {
-        console.error('Registration failed', error);
-      })
-    );
+    return this.http
+      .post(`${this.API_URL}/register`, { username, password })
+      .pipe(
+        tap((response) => {
+          console.log('Registration successful', response);
+        }),
+        tap((error) => {
+          console.error('Registration failed', error);
+        })
+      );
   }
 
   login(email: string, password: string): Observable<any> {
     const username = email.split('@')[0];
     return this.http
-      .post('{API_URL}/login', {
+      .post(`${this.API_URL}/login_check`, {
         username,
         password,
       })
       .pipe(
         tap((response: any) => {
-          console.log('Login successful', response);
-          localStorage.setItem('token', response.token);
-          localStorage.setItem('refresh_token', response.refresh_token);
+          console.log('Login successful', response.token);
+          this.storageService.setItem('token', response.token);
+          this.storageService.setItem('refresh_token', response.refresh_token);
 
           const payload = JSON.parse(atob(response.token.split('.')[1]));
           const user = { username: payload.username, roles: payload.roles };
-          localStorage.setItem('user', JSON.stringify(user));
+          this.storageService.setItem('user', JSON.stringify(user));
           this.currentUserSubject.next(user);
         }),
         catchError((error) => {
