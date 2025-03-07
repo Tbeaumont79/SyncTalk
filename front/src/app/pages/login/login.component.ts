78d8b7b4c166b9b1bebba618e3b988d089e2e66e
tbeaumont79 <tbeaumont79@icloud.com>
2025-03-07 17:17:34 +0100

    feat: login now redirect to the homePage when user is successfully loged

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/authentification/auth.service';
 import { throwError, catchError } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,19 +14,25 @@ export class LoginComponent {
   email = '';
   password = '';
   errorMessage = '';
-  constructor(private authService: AuthService) {}
+  constructor(
+    @Inject(AuthService) private authService: AuthService,
+    @Inject(Router) private router: Router
+  ) {}
 
   login() {
     this.authService
       .login(this.email, this.password)
       .pipe(
         catchError((error) => {
-          this.errorMessage = "Invalid username or password";
+          this.errorMessage = 'Invalid username or password';
           return throwError(() => error);
         })
       )
       .subscribe({
-        next: (response) => console.log('Login successful', response),
+        next: (response) => {
+          console.log('Login successful', response);
+          this.router.navigate(['/']);
+        },
         error: (err) => console.error('Login failed', err),
       });
     console.log(this.email, this.password);
